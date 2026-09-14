de7b8d2998d03c59230c9acdf6e2cc5b881d8bda
James Fregeau <jtfreg777@gmail.com>
2026-09-14 01:54:19 -0700

    Added LRU eviction and max size to the cache

--- caching_proxy.py
+++ caching_proxy.py
@@ -6,7 +6,6 @@ from aiohttp import web
 import requests
 from multidict import CIMultiDict
 
-
 HOP_BY_HOP_HEADERS = ("server", "connection", "keep-alive","te", 
                           "transfer-encoding", "trailer", "upgrade", 
                           "proxy-authenticate", "proxy-authorization",
@@ -15,9 +14,10 @@ HOP_BY_HOP_HEADERS = ("server", "connection", "keep-alive","te",
 
 class Handler(web.View):
     url: str = "https://dummyjson.com"
-    cache = {} 
+    lru_cache = {} 
     session: aiohttp.ClientSession
-
+    max_size: int = -1
+    
     @classmethod
     def set_url(cls, url):
         if not cls.url:
@@ -28,12 +28,20 @@ class Handler(web.View):
         cls.session = aiohttp.ClientSession()
         yield
         await cls.session.close()
-        
+
+    @classmethod
+    def set_size(cls, max_size):
+        cls.max_size = max_size
+
     async def get(self) -> web.Response:
-        r= Handler.cache.get(self.request.rel_url, None)
-        print(len(Handler.cache.keys()))
+        r = Handler.lru_cache.pop(self.request.rel_url, None)
+
         if r is not None:  
+            # Add it back to the to the most recent used
+            Handler.lru_cache[self.request.rel_url] = r
+            print(f"{self.request.rel_url} Cache hit \n")
             return web.Response(status=r[0], headers=r[1], body=r[2])
+        
         full_url = Handler.url + str(self.request.rel_url)
         return await self.fetch(full_url)
 
@@ -45,12 +53,20 @@ class Handler(web.View):
 
         for h in HOP_BY_HOP_HEADERS:
             headers.popall(h, None)
-
-        Handler.cache[self.request.rel_url] = (status, headers, body)
+        # Just in case it is in the cache
+        Handler.lru_cache.pop(self.request.rel_url, None)
+        Handler.lru_cache[self.request.rel_url] = (status, headers, body)
+        print(f"{url} added to cache")
+        if Handler.max_size != - 1 and len(Handler.lru_cache) > Handler.max_size:
+            lru = next(iter(Handler.lru_cache))
+            Handler.lru_cache.pop(lru)
+            print(f"{lru} removed from cache")
+        print(f"Cache: {len(Handler.lru_cache)}/{Handler.max_size} \n")
         return web.Response(status=status, headers=headers, body=body)
     
 if __name__ =="__main__":
     server = web.Application()
     server.router.add_route("*", "/{tail:.*}", Handler)
+    Handler.set_size(10)
     server.cleanup_ctx.append(Handler.setup)
     web.run_app(server, host="localhost", port=3000)
